decd615e6abf840e286a9c8ab7b8b0684e7d5b74
Daniel-1-2-3 <danielzhu501@gmail.com>
2023-11-20 15:53:57 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// importing modules: discord.js, and reading from console
+const {Client, GatewayIntentBits} = require('discord.js');
+const readLine = require('readline') 
+
+const client = new Client({ // intents are a set of permissions that the bot can use, like recieving messages, access to members names, etc.
+    intents: [
+        GatewayIntentBits.Guilds, // guilds = server
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is ready`); // using (`) NOT )(')
+});
+
+client.on('messageCreate', async (message) => {
+    const channel = message.guild.channels.cache.get('1176246282617102370');
+    if(message.content.toLowerCase() == 'active?'){
+        channel.send('Active');
+    }
+});
+
+client.login('MTE3NjI0NTk0NTY5NzA1NDc5MA.GVQ3mE.RamNmS83HQM9Eq2IFzRPiojpm4asS6XxEcyO0c');
+//Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass (to bypass the policy preventing use of nodemon)
